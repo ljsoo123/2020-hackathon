3e8a4499faf0c53f1b714c3769f2deb08409c7b9
ljsoo123 <ljsoo123456@naver.com>
2020-12-01 18:59:42 +0900

    [CREATE] header style

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const TextDiv = styled.div`
+  @font-face {
+    font-family: "HangeulNuri-Bold";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_three@1.0/HangeulNuri-Bold.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
+  font-family: HangeulNuri-Bold;
+  text-align: center;
+  font-size: 50px;
+  margin-top: 50px;
+`;
